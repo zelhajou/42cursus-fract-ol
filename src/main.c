04cb4be7f21443f000483d30721fe1cc5d02e4ec
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-21 02:02:28 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,39 +6,18 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 22:30:50 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/20 00:37:55 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/21 01:31:36 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-#define MALLOC_ERROR 1
-#define WIDTH 400
-#define HEIGHT 400
-
+#include "graphics.h"
 
 int	main(void)
 {
-	void	*mlx_connextion;
-	void	*mlx_window;
+	t_gfx_env	env;
 
-	// initailize the mlx connextion
-	mlx_connextion = mlx_init();
-	// check if the connextion is created
-	if (!mlx_connextion)
-		return (MALLOC_ERROR);
+	ft_setup_window(&env);
 
-	// create a window
-	mlx_window = mlx_new_window(mlx_connextion, HEIGHT, WIDTH, "My window");
-	// check if the window is created
-	if (!mlx_window)
-	{	
-		mlx_destroy_window(mlx_connextion, mlx_window);
-		return (MALLOC_ERROR);
-	}
-	
-	// loop to keep the window open
-	mlx_loop(mlx_connextion);
-	
 	return (0);
-	}
+}
