0908db46832ff711e131ec7ca99393e9f698c6bc
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-24 18:00:21 +0100

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:10:26 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/21 01:31:33 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/23 17:16:47 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,23 @@
 
 #include <mlx.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
 
-#define WIDTH 800
-#define HEIGHT 600
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 typedef struct s_gfx_env {
 	void *mlx_connextion;
 	void *mlx_window;
 }	t_gfx_env;
 
+
 void	ft_setup_window(t_gfx_env *env);
+int		ft_render(t_gfx_env *env);
+int		ft_change_color(t_gfx_env *env);
+int		ft_close_window(t_gfx_env *env);
 
 #endif
