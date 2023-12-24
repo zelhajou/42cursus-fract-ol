f9e9c21e1aa3c7479b4b411d6e4b5715bf6db06e
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-24 17:59:57 +0100

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -6,26 +6,40 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:06:53 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/21 01:49:03 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/23 18:00:41 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "graphics.h"
+#include "events.h"
+#include "drawing.h"
+
+int ft_close_window(t_gfx_env *env) {
+    mlx_destroy_window(env->mlx_connextion, env->mlx_window);
+	free(env->mlx_connextion);
+    exit(0);
+}
+
 
 void	ft_setup_window(t_gfx_env *env)
 {
 	env->mlx_connextion = mlx_init();
 	if (!env->mlx_connextion)
 		exit(1);
-	env->mlx_window = mlx_new_window(env->mlx_connextion, HEIGHT, WIDTH, "Fractol");
+	env->mlx_window = mlx_new_window(env->mlx_connextion, WINDOW_HEIGHT, WINDOW_WIDTH, "Fractol");
 	if (!env->mlx_window)
 	{
 		mlx_destroy_window(env->mlx_connextion, env->mlx_window);
 		free(env->mlx_connextion);
 		exit(1);
 	}
-	mlx_loop(env->mlx_connextion);
+	ft_setup_hooks(env);
+	ft_render_fractal(env);
+	
+	mlx_loop_hook(env->mlx_connextion, ft_render_fractal, env);
 
+
+	mlx_loop(env->mlx_connextion);
 	mlx_destroy_window(env->mlx_connextion, env->mlx_window);
 	// mlx_destroy_display(env->mlx_connextion);
 	free(env->mlx_connextion);
