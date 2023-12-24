3d66c33bbe045a25363c0d1c5511fc878f9557ab
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-24 18:00:43 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,18 +6,20 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 22:30:50 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/21 01:31:36 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/23 17:27:44 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fractol.h"
+#include "fractal.h"
 #include "graphics.h"
+#include "events.h"
 
 int	main(void)
 {
 	t_gfx_env	env;
 
 	ft_setup_window(&env);
+
 
 	return (0);
 }
