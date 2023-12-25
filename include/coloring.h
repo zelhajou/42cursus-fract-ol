fea5f99a34f67652f4bd4e0af4bec872bc1c6e4f
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-25 21:58:35 +0100

    Update coloring.h

--- include/coloring.h
+++ include/coloring.h
@@ -6,7 +6,17 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 16:58:06 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/23 16:58:07 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/25 10:32:48 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef COLORING_H
+#define COLORING_H
+
+typedef struct s_color {
+    int red;
+    int green;
+    int blue;
+} t_color;
+
+#endif
