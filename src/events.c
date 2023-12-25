06ffa5c965f904975cea4e9c9c1ab2fa58eeeeaa
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-25 22:16:39 +0100

    Update events.c

--- src/events.c
+++ src/events.c
@@ -6,11 +6,13 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 22:15:52 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/23 15:02:53 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/25 22:09:29 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "events.h"
+#include "graphics.h"
+#include "drawing.h"
 
 int ft_handle_keypress(int key, t_gfx_env *env)
 {
@@ -29,13 +31,12 @@ int ft_handle_keypress(int key, t_gfx_env *env)
 
 int	ft_handle_mouse(int button, int x, int y, t_gfx_env *env)
 {
-	(void)env;
 	if (button == ZOOM_IN_KEY)
-		printf("ZOOM_IN_KEY %d pressed at %d:%d\n", button, x, y);
+		env->mouse.zoom *= 1.1;
 	else if (button == ZOOM_OUT_KEY)
-	    printf("ZOOM_OUT_KEY %d pressed at %d:%d\n", button, x, y);
+		env->mouse.zoom /= 1.1;
 	else if (button == MOUSE_LEFT_CLICK)
-	    printf("MOUSE_LEFT_CLICK %d pressed at %d:%d\n", button, x, y);
+		printf("MOUSE_LEFT_CLICK %d pressed at %d:%d\n", button, x, y);
 	else if (button == MOUSE_RIGHT_CLICK)
 	    printf("MOUSE_RIGHT_CLICK %d pressed at %d:%d\n", button, x, y);
 	else if (button == MOUSE_MIDDLE_CLICK)
@@ -43,8 +44,10 @@ int	ft_handle_mouse(int button, int x, int y, t_gfx_env *env)
 	return (0);
 }
 
-int ft_handle_mouse_move(int x, int y)
+int ft_handle_mouse_move(int x, int y, t_gfx_env *env)
 {
+	env->mouse.x = x;
+	env->mouse.y = y;	
 	printf("Mouse moved to %d:%d\n", x, y);
 	return (0);
 }
@@ -54,7 +57,6 @@ void ft_setup_hooks(t_gfx_env *env)
 	mlx_key_hook(env->mlx_window, ft_handle_keypress, env);
 	mlx_mouse_hook(env->mlx_window, ft_handle_mouse, env);
 	mlx_hook(env->mlx_window, 17, 0, ft_close_window, env);
-	mlx_hook(env->mlx_window, 6, 0, ft_handle_mouse_move, env); // 6 is the event number for mouse movement in MiniLibX
-
-	//mlx_loop_hook(env->mlx_connextion, ft_render, env);
+	mlx_hook(env->mlx_window, 6, 0, ft_handle_mouse_move, env);
+	mlx_loop_hook(env->mlx_connextion, ft_render_fractal, env);
 }
