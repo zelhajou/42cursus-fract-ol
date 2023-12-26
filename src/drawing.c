1567bfaed1a15152bea761f36407412640f521b5
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-27 00:04:45 +0100

    Update drawing.c

--- src/drawing.c
+++ src/drawing.c
@@ -6,78 +6,110 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 16:57:43 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/25 22:15:40 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/27 00:03:57 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "graphics.h"
-#include "drawing.h"
-#include <math.h>
+#include "fractol.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	put_pixel_to_image(t_fractol *fractol, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT) {
+		dst = fractol->image.addr + (y * fractol->image.line_length
+				+ x * (fractol->image.bits_per_pixel / 8));
+		*(unsigned int *)dst = color;
+	}
 }
 
-void ft_put_pixel(t_data *data, int x, int y, int color)
-{
-    char *pxl;
+// Scaling pixel_to_complex
+double	ft_scale_coordinate_x(t_fractol *fractol, int x) {
+    double half_width = WINDOW_WIDTH / 2.0;
+    return ((x - half_width) * (4.0 * fractol->zoom) / WINDOW_WIDTH);
+}
 
-    if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
-    {
-        pxl = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-        *(unsigned int *)pxl = color;
+double	ft_scale_coordinate_y(t_fractol *fractol, int y) {
+    double half_height = WINDOW_HEIGHT / 2.0;
+    return (((WINDOW_HEIGHT - y) - half_height)
+			* (4.0 * fractol->zoom) / WINDOW_HEIGHT);
+}
+
+void	ft_setup_fractal_params(t_fractol *fractol)
+{
+	double half_width = WINDOW_WIDTH / 2.0;
+    double half_height = WINDOW_HEIGHT / 2.0;
+
+    fractol->c.real = (fractol->offset.x - half_width)
+		* (4.0 * fractol->zoom) / WINDOW_WIDTH;
+    fractol->c.imaginary = (half_height - fractol->offset.y)
+		* (4.0 * fractol->zoom) / WINDOW_HEIGHT;
+}
+
+void	ft_initialize_image(t_fractol *fractol)
+{
+	fractol->image.img_ptr = mlx_new_image(&fractol->image,
+			WINDOW_HEIGHT, WINDOW_WIDTH);
+	  if (!fractol->image.img_ptr) {
+        // 
+        exit(EXIT_FAILURE);
+    }
+	fractol->image.addr = mlx_get_data_addr(fractol->image.img_ptr,
+			&fractol->image.bits_per_pixel, &fractol->image.line_length,
+			&fractol->image.endian);
+	 if (!fractol->image.addr) {
+        //
+        exit(EXIT_FAILURE);
     }
 }
 
-struct s_complex{
-	double real;
-	double imag;
-};
+int determine_color(int iterations, int max_iterations) {
+    int color;
+    double t;
 
-int function(struct s_complex c, struct s_complex z, int max_iterations)
-{
-	while (max_iterations)
-	{
-		double r = z.real * z.real - z.imag * z.imag + c.real;
-		double i = 2 * z.real * z.imag + c.imag;
-		z.real = r;
-		z.imag = i;
-		if (z.real * z.real + z.imag * z.imag > 4)
-			return max_iterations;
-		max_iterations--;
-	}
-	return (max_iterations);
+    if (iterations == max_iterations) {
+        color = 0x000000;
+    } else {
+        t = (double)iterations / max_iterations;
+        color = (int)(9 * (1 - t) * t * t * t * 255) << 16 |
+                (int)(15 * (1 - t) * (1 - t) * t * t * 255) << 8 |
+                (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+    }
+    return color;
 }
 
-
-int	ft_render_fractal(t_gfx_env *env)
+void	draw_fractal_pixel(t_fractol *fractol, int x, int y)
 {
-	t_data	img;
-	int x, y, color = 0;
-	struct s_complex n;
-	struct s_complex c;
+    int color;
+	int iterations;
 
-	c.real = (env->mouse.x - WINDOW_WIDTH / 2.0) * (4.0 * env->mouse.zoom) / WINDOW_WIDTH;;
-	c.imag = ((WINDOW_HEIGHT - env->mouse.y) - WINDOW_HEIGHT / 2.0) * (4.0 * env->mouse.zoom) / WINDOW_HEIGHT;;
-	// int color = rand() % 0xFFFFFF;
-	img.img = mlx_new_image(&img, WINDOW_HEIGHT, WINDOW_WIDTH);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	// &img.bits_per_pixel, &img.line_length, &img.endian);
+	color = 0;
+	fractol->z.real = ft_scale_coordinate_x(fractol, x);
+	fractol->z.imaginary = ft_scale_coordinate_y(fractol, y);
+	iterations = calculate_mandelbrot(fractol, fractol->max_iterations);
 
-	for (y = 0; y < WINDOW_WIDTH; y++) {
-        for (x = 0; x < WINDOW_HEIGHT; x++) {
-			n.real = (x - WINDOW_WIDTH / 2.0) * (4.0 * env->mouse.zoom) / WINDOW_WIDTH;
-			n.imag = ((WINDOW_HEIGHT - y) - WINDOW_HEIGHT / 2.0) * (4.0 * env->mouse.zoom) / WINDOW_HEIGHT;
-			color = function(c, n, 100);
-			my_mlx_pixel_put(&img, x, y, color * 0x0608F2);
+	color = determine_color(iterations, fractol->max_iterations);
+	put_pixel_to_image(fractol, x, y, color);
+}
+
+int	ft_render_fractal(t_fractol *fractol)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	ft_setup_fractal_params(fractol);
+	ft_initialize_image(fractol);
+	while (y < WINDOW_WIDTH)
+	{
+		x = 0;
+		while (x < WINDOW_HEIGHT)
+		{
+			draw_fractal_pixel(fractol, x, y);
+			x++;
 		}
+		y++;
 	}
-	mlx_put_image_to_window(env->mlx_connextion, env->mlx_window, img.img, 0, 0);
-	//mlx_string_put(env->mlx_connextion, env->mlx_window, 5, 5, 0, "Fractol");
+	mlx_put_image_to_window(fractol->mlx_connextion, fractol->mlx_window, fractol->image.img_ptr, 0, 0);
 	return (0);
 }
