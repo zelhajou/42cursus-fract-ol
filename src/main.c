70bc82a9d92bfdefba20158a8220723c1e536a4c
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-27 00:04:50 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,20 +6,36 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 22:30:50 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/23 17:27:44 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:52:24 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fractal.h"
-#include "graphics.h"
-#include "events.h"
+#include "fractol.h"
+
+// t_fractol *init_fractal(void) {
+//     t_fractol *fractol = malloc(sizeof(t_fractol));
+//     if (!fractol) return NULL;
+// ****************************************************************
+// 	// fractol->zoom = 1;
+	// fractol->offset.x = 0;
+	// fractol->offset.y = 0;
+	// fractol->max_iterations = 100;
+//     return fractol;
+// }
 
 int	main(void)
 {
-	t_gfx_env	env;
+	t_fractol	fractol;
 
-	ft_setup_window(&env);
+	ft_setup_window(&fractol);
 
+	fractol.zoom = 1;
+	fractol.max_iterations = 100;
+	ft_setup_hooks(&fractol);
+	ft_render_fractal(&fractol);
+	mlx_loop(fractol.mlx_connextion);
+	mlx_destroy_window(fractol.mlx_connextion, fractol.mlx_window);
+	free(fractol.mlx_connextion);
 
 	return (0);
 }
