9ada1c378c52686fd43fac27e1582a282eb5062a
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-26 09:50:40 +0100

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:10:26 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/23 17:16:47 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/25 22:14:47 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,22 @@
 
 
 #define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_HEIGHT 800
+
+typedef struct s_point {
+    int x; // X coordinate
+    int y; // Y coordinate
+	double zoom;
+}	t_point;
 
 typedef struct s_gfx_env {
 	void *mlx_connextion;
 	void *mlx_window;
-}	t_gfx_env;
-
+	t_point mouse;
+} t_gfx_env;
 
 void	ft_setup_window(t_gfx_env *env);
 int		ft_render(t_gfx_env *env);
-int		ft_change_color(t_gfx_env *env);
 int		ft_close_window(t_gfx_env *env);
 
 #endif
