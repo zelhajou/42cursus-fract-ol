8fbd62b3b988b97637d8a5194cf10febd3f46ea0
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-27 00:01:43 +0100

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -6,37 +6,36 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:06:53 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/25 22:07:49 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:31:11 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "graphics.h"
-#include "events.h"
-#include "drawing.h"
+#include "fractol.h"
 
-int ft_close_window(t_gfx_env *env) {
-    mlx_destroy_window(env->mlx_connextion, env->mlx_window);
-	free(env->mlx_connextion);
-    exit(0);
+
+int	ft_close_window(t_fractol *fractol)
+{
+	mlx_destroy_window(fractol->mlx_connextion, fractol->mlx_window);
+	free(fractol->mlx_connextion);
+	exit(0);
 }
 
-void	ft_setup_window(t_gfx_env *env)
-{
-	env->mlx_connextion = mlx_init();
-	if (!env->mlx_connextion)
+void ft_init_window(t_fractol *fractol) {
+	fractol->mlx_connextion = mlx_init();
+	if (!fractol->mlx_connextion)
 		exit(1);
-	env->mlx_window = mlx_new_window(env->mlx_connextion, WINDOW_HEIGHT, WINDOW_WIDTH, "Fractol");
-	if (!env->mlx_window)
+	fractol->mlx_window = mlx_new_window(fractol->mlx_connextion,
+			WINDOW_HEIGHT, WINDOW_WIDTH, "Fractol");
+	if (!fractol->mlx_window)
 	{
-		mlx_destroy_window(env->mlx_connextion, env->mlx_window);
-		free(env->mlx_connextion);
+		mlx_destroy_window(fractol->mlx_connextion, fractol->mlx_window);
+		free(fractol->mlx_connextion);
 		exit(1);
 	}
-	env->mouse.zoom = 1;
-	ft_setup_hooks(env);
-	ft_render_fractal(env);
-	mlx_loop(env->mlx_connextion);
-	mlx_destroy_window(env->mlx_connextion, env->mlx_window);
-	// mlx_destroy_display(env->mlx_connextion);
-	free(env->mlx_connextion);
+}
+
+void	ft_setup_window(t_fractol *fractol)
+{
+	ft_init_window(fractol);
+	
 }
