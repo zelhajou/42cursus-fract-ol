be7d7b09a617331dc02bb3becbf92f8637d5d5b8
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-27 17:42:30 +0100

    Create fractal.c

---
+++ src/fractal.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/27 17:26:30 by zelhajou          #+#    #+#             */
+/*   Updated: 2023/12/27 17:40:29 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	ft_calculate_julia(t_fractol *fractol, int max_iterations)
+{
+	return (ft_iterate_fractal(fractol->z, fractol->c, max_iterations));
+}
+
+int	ft_calculate_mandelbrot(t_fractol *fractol, int max_iterations)
+{
+	fractol->c.real = 0;
+	fractol->c.imaginary = 0;
+	return (ft_iterate_fractal(fractol->c, fractol->z, max_iterations));
+}
+
+int	ft_iterate_fractal(t_complex z, t_complex c, int max_iterations)
+{
+	int		iterations;
+	double	real;
+	double	imaginary;
+
+	iterations = 0;
+	while (iterations < max_iterations)
+	{
+		real = z.real * z.real - z.imaginary * z.imaginary + c.real;
+		imaginary = 2 * z.real * z.imaginary + c.imaginary;
+		z.real = real;
+		z.imaginary = imaginary;
+		if (z.real * z.real + z.imaginary * z.imaginary > 4)
+			break ;
+		iterations++;
+	}
+	return (iterations);
+}
+
+int	ft_render_fractal(t_fractol *fractol)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	ft_setup_fractal_params(fractol);
+	ft_initialize_image(fractol);
+	while (y < WINDOW_WIDTH)
+	{
+		x = 0;
+		while (x < WINDOW_HEIGHT)
+		{
+			ft_draw_fractal_pixel(fractol, x, y);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(fractol->mlx_connextion, fractol->mlx_window,
+		fractol->image.img_ptr, 0, 0);
+	return (0);
+}
