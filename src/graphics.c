d84841473a44e02a6cd82590dbbd14c8406542e5
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-27 17:42:42 +0100

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -6,21 +6,19 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:06:53 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/26 22:31:11 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/27 16:36:37 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-
-int	ft_close_window(t_fractol *fractol)
+void	ft_setup_window(t_fractol *fractol)
 {
-	mlx_destroy_window(fractol->mlx_connextion, fractol->mlx_window);
-	free(fractol->mlx_connextion);
-	exit(0);
+	ft_init_window(fractol);
 }
 
-void ft_init_window(t_fractol *fractol) {
+void	ft_init_window(t_fractol *fractol)
+{
 	fractol->mlx_connextion = mlx_init();
 	if (!fractol->mlx_connextion)
 		exit(1);
@@ -34,8 +32,26 @@ void ft_init_window(t_fractol *fractol) {
 	}
 }
 
-void	ft_setup_window(t_fractol *fractol)
+int	ft_close_window(t_fractol *fractol)
 {
-	ft_init_window(fractol);
-	
+	mlx_destroy_window(fractol->mlx_connextion, fractol->mlx_window);
+	free(fractol->mlx_connextion);
+	exit(EXIT_FAILURE);
+}
+
+void	ft_initialize_image(t_fractol *fractol)
+{
+	fractol->image.img_ptr = mlx_new_image(&fractol->image,
+			WINDOW_HEIGHT, WINDOW_WIDTH);
+	if (!fractol->image.img_ptr)
+	{
+		exit(EXIT_FAILURE);
+	}
+	fractol->image.addr = mlx_get_data_addr(fractol->image.img_ptr,
+			&fractol->image.bits_per_pixel, &fractol->image.line_length,
+			&fractol->image.endian);
+	if (!fractol->image.addr)
+	{
+		exit(EXIT_FAILURE);
+	}
 }
