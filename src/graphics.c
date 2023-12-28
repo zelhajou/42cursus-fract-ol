2143ed64719886d84a2fa386a140019204f662c2
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-28 15:07:47 +0100

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 00:06:53 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/27 16:36:37 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/28 15:02:44 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,11 @@ void	ft_initialize_image(t_fractol *fractol)
 	fractol->image.img_ptr = mlx_new_image(&fractol->image,
 			WINDOW_HEIGHT, WINDOW_WIDTH);
 	if (!fractol->image.img_ptr)
-	{
-		exit(EXIT_FAILURE);
-	}
+		ft_close_window(fractol);
 	fractol->image.addr = mlx_get_data_addr(fractol->image.img_ptr,
-			&fractol->image.bits_per_pixel, &fractol->image.line_length,
+			&fractol->image.bits_per_pixel,
+			&fractol->image.line_length,
 			&fractol->image.endian);
 	if (!fractol->image.addr)
-	{
-		exit(EXIT_FAILURE);
-	}
+		ft_close_window(fractol);
 }
