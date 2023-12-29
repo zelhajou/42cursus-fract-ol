a6e938ec73cf16a3e27292b3a945588f68a68309
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2023-12-29 18:23:46 +0100

    Update events.c

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 22:15:52 by zelhajou          #+#    #+#             */
-/*   Updated: 2023/12/28 20:07:24 by zelhajou         ###   ########.fr       */
+/*   Updated: 2023/12/29 17:21:55 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,31 @@ int	ft_handle_keypress(int key, t_fractol *fractol)
 {
 	if (key == KEY_ESC)
 		ft_close_window(fractol);
-	else if (key == KEY_LEFT)
-		printf("LEFT pressed\n");
-	else if (key == KEY_RIGHT)
-		printf("RIGHT pressed\n");
-	else if (key == KEY_UP)
-		printf("UP pressed\n");
-	else if (key == KEY_DOWN)
-		printf("DOWN pressed\n");
+	// else if (key == KEY_LEFT)
+	// 	printf("LEFT pressed\n");
+	// else if (key == KEY_RIGHT)
+	// 	printf("RIGHT pressed\n");
+	// else if (key == KEY_UP)
+	// 	printf("UP pressed\n");
+	// else if (key == KEY_DOWN)
+	// 	printf("DOWN pressed\n");
 	return (0);
 }
 
 int	ft_handle_mouse(int button, int x, int y, t_fractol *fractol)
 {
+	(void)x;
+	(void)y;
 	if (button == ZOOM_IN_KEY)
 		fractol->zoom *= 1.1;
 	else if (button == ZOOM_OUT_KEY)
 		fractol->zoom /= 1.1;
-	else if (button == MOUSE_LEFT_CLICK)
-		printf("MOUSE_LEFT_CLICK %d pressed at %d:%d\n", button, x, y);
-	else if (button == MOUSE_RIGHT_CLICK)
-		printf("MOUSE_RIGHT_CLICK %d pressed at %d:%d\n", button, x, y);
-	else if (button == MOUSE_MIDDLE_CLICK)
-		printf("MOUSE_MIDDLE_CLICK %d pressed at %d:%d\n", button, x, y);
+	// else if (button == MOUSE_LEFT_CLICK)
+	// 	printf("MOUSE_LEFT_CLICK %d pressed at %d:%d\n", button, x, y);
+	// else if (button == MOUSE_RIGHT_CLICK)
+	// 	printf("MOUSE_RIGHT_CLICK %d pressed at %d:%d\n", button, x, y);
+	// else if (button == MOUSE_MIDDLE_CLICK)
+	// 	printf("MOUSE_MIDDLE_CLICK %d pressed at %d:%d\n", button, x, y);
 	return (0);
 }
 
